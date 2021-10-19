7bdbad61d6a6c832ed57dfd715ae000aadca73ba
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-19 17:00:39 +0100

    Added Java exercises files

--- dfesDay1/src/com/qa/main/Turtle.java
+++ dfesDay1/src/com/qa/main/Turtle.java
@@ -1,27 +1,31 @@
 package com.qa.main;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
-public class Turtle {
+class Turtle {
 
-	public static void main(String[] args) {
+	
+		public String shellColour = "Shiny Grey";
+		public float weight = 286.3f;
+		public boolean isOld = true;
 		
-		String shellColour = "Shiny Grey";
-		float weight = 286.3f;
-		boolean isOld = true;
-		
-		List<String> favouriteFoods = new ArrayList<>();
-		
-		favouriteFoods.add("lettuce");
-		favouriteFoods.add("watermelon");
-		favouriteFoods.add("strawberries");
-
-		System.out.println(favouriteFoods);	
+//		List<String> favouriteFoods = new ArrayList<>();
+//		
+//		favouriteFoods.add("lettuce");
+//		favouriteFoods.add("watermelon");
+//		favouriteFoods.add("strawberries");
+//
+//		System.out.println(favouriteFoods);	
 		
 //		public void swim() {
 //			System.out.println("I'm a natural swimmer");
-//			
-//	}
+//		public Turtle getObject() {
+	
+		Turtle() {
+			this.shellColour = shellColour;
+		}
+		
+	}
 
-}
+
