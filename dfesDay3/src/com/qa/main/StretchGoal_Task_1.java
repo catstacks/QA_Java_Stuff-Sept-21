7734c0fd47ce6b30cb8c35b17bbaa14bde2152dd
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-20 10:36:06 +0100

    Added More Day 3 materials

---
+++ dfesDay3/src/com/qa/main/StretchGoal_Task_1.java
@@ -0,0 +1,17 @@
+package com.qa.main;
+
+import java.util.Scanner;
+
+public class StretchGoal_Task_1 {
+
+	public static void numInputs() {
+		// Stretch goal - Community, Java Beginner -> conditonals -> flow chart exercise
+        Scanner numScan = new Scanner(System.in);
+        System.out.println("Input an integer: ");
+
+        int A = numScan.nextInt();
+        
+        
+	}
+
+}
