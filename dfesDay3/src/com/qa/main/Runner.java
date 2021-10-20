15290f8cf46ab97aae5e06b413a1b447a940fb8b
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-20 13:09:42 +0100

    Added do while exercises

--- dfesDay3/src/com/qa/main/Runner.java
+++ dfesDay3/src/com/qa/main/Runner.java
@@ -4,8 +4,10 @@ public class Runner {
 
 	public static void main(String[] args) {
 		//Task_1.ifElse();
-		FizzBuzz.fuzzy(1);
-		Task_2.moneyLoop();
+		//FizzBuzz.fuzzy(1);
+		//Task_2.moneyLoop();
+		Task_2_Extension.walletLoop();
+		
 
 	}
 
---
+++ dfesDay3/src/com/qa/main/Task_2_Extension.java
@@ -0,0 +1,22 @@
+package com.qa.main;
+
+public class Task_2_Extension {
+
+	public static void walletLoop() {
+		
+		int total = 300;
+		
+		
+		do {
+			total -= 45;
+			//System.out.println("Your starting balance is: " + total);
+		} while(total > 87); {
+			System.out.println("Your current total is: " + total);
+			}	
+				
+		if(total < 87) {
+			System.out.println("You're spending too much money!");
+			}			
+
+		}
+	}	
