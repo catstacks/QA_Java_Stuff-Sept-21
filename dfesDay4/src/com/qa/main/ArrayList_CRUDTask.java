673732cebd9b0798a70d57aa751651745a9f3239
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-21 11:24:29 +0100

    Create ArrayList_CRUDTask.java

---
+++ dfesDay4/src/com/qa/main/ArrayList_CRUDTask.java
@@ -0,0 +1,41 @@
+package com.qa.main;
+
+import java.util.ArrayList;
+
+public class ArrayList_CRUDTask {
+
+	public static void QA_CAfe() {
+
+//		Exercise - You are running a succesful cafe called QA-Cafe. 
+//		With a list of orders, create methods with paramaters that can
+//		do the following:
+//
+//		2 ArrayLists, one doneOrders and orders
+//
+//			- Add a new order to an orders array
+//			- Move an existing order to a 'Done orders array'
+//			- Return the name of an order
+//			- Modify the order by index with a new name
+//			- Remove the order from either arrays
+//			- Print the length of either arrays
+//			- Clear either arrays
+		
+		public static ArrayList<String> orders = new ArrayList<>();
+		
+		public static void setupArray() {
+		
+		orders.add("Small Espresso");
+		orders.add("Large Cold Brew");
+		orders.add("Medium Cappuccino");
+		orders.add("Large Choc Cookie Frappe");
+		orders.add("Small Caramel Iced Latte");
+		orders.add("Small Americano");
+		orders.add("lettuce");
+		orders.add("Large Cafe Au Lait");
+		orders.add("Medium Strawberry Iced Tea");
+		
+
+		System.out.println(orders);
+	}
+
+}
