a9e1edb2e4971e89e1699398c168f7fcc8f13812
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-21 11:59:39 +0100

    Update ArrayList_CRUDTask.java

--- dfesDay4/src/com/qa/main/ArrayList_CRUDTask.java
+++ dfesDay4/src/com/qa/main/ArrayList_CRUDTask.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 
 public class ArrayList_CRUDTask {
 
-	public static void QA_CAfe() {
-
-//		Exercise - You are running a succesful cafe called QA-Cafe. 
-//		With a list of orders, create methods with paramaters that can
+//		Exercise - You are running a successful cafe called QA-Cafe. 
+//		With a list of orders, create methods with parameters that can
 //		do the following:
 //
 //		2 ArrayLists, one doneOrders and orders
@@ -21,21 +19,40 @@ public class ArrayList_CRUDTask {
 //			- Clear either arrays
 		
 		public static ArrayList<String> orders = new ArrayList<>();
+		public static ArrayList<String> doneOrders = new ArrayList<>();
 		
-		public static void setupArray() {
+		public static void addOrder(String newOrder) {		
+			orders.add("Small Espresso");
+			orders.add("Large Cold Brew");
+			orders.add("Medium Cappuccino");
+			orders.add("Large Choc Cookie Frappe");
+			orders.add("Small Caramel Iced Latte");
+			orders.add("Small Americano");
+			orders.add("lettuce");
+			orders.add("Large Cafe Au Lait");
+			orders.add("Medium Strawberry Iced Tea");
+		}
 		
-		orders.add("Small Espresso");
-		orders.add("Large Cold Brew");
-		orders.add("Medium Cappuccino");
-		orders.add("Large Choc Cookie Frappe");
-		orders.add("Small Caramel Iced Latte");
-		orders.add("Small Americano");
-		orders.add("lettuce");
-		orders.add("Large Cafe Au Lait");
-		orders.add("Medium Strawberry Iced Tea");
+		public static void moveOrder(int index) {
+			
+			System.out.println(orders);
+			
+			orders.add(null);
+		}
 		
-
-		System.out.println(orders);
+		public static void removeOrder(int index) {
+			
+			System.out.println(orders);
+			
+			orders.add(null);
+		}
+		
+		public static void updateOrder(int index, String newOrder) {
+			
+			System.out.println(orders);
+			
+			orders.add(null);
+		}
 	}
 
-}
+
