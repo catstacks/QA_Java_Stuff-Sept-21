96852b1549de06a398cc1bc8793b83192bd75a53
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-22 15:11:04 +0100

    Create garage

---
+++ dfesDay5/src/com/qa/garage/Garage.java
@@ -0,0 +1,63 @@
+package com.qa.garage;
+
+import java.util.ArrayList;
+
+public class Garage {
+
+	public static void main(String[] args) {
+		
+		public static ArrayList<Vehicle> addVehicle = new ArrayList<>();
+
+		
+		public static void addVehicle(Vehicle newVehicle) {		
+			addVehicle.add();
+		
+		//}
+		
+		//public static void removeVehicle(int index) {
+			
+			//arrayname.add(null);
+		//}
+		
+		//public static void updateVehicle(int index, String newOrder) {
+		
+		//System.out.println(orders);
+		
+		//orders.add(null);
+	//}
+		
+		//public static void readVehicle(int index, String newOrder) {
+		
+		//System.out.println(orders);
+		
+		//orders.add(null);
+	//}
+		
+		//public static void removeAllVehicle(int index) {
+			
+			//System.out.println(orders);
+			
+			//orders.add(null);
+		//}
+		
+
+	//}
+
+	}
+
+}
+
+//The task is to create a garage class with methods for the following:
+//- Adding Vehicles to garage
+//- Removing vehicles
+//- Updating vehicles
+//- Reading vehicle
+//- Removing all vehicles
+//- Running vehicle.fixVehicle() custom method
+//Vehicles
+//Vehicle should be a base class (parent) with at least 3 fields
+//and a fixVehicle() method
+//
+//You should create at least 3 child classes extending off of Vehicle 
+//(car, bike, helicopter)
+//Each child class should inherit the fields AND have 2 specific fields
--- dfesDay5/src/com/qa/garage/Vehicle.java
+++ dfesDay5/src/com/qa/garage/Vehicle.java
@@ -1,7 +1,5 @@
 package com.qa.garage;
 
-import java.util.ArrayList;
-
 public class Vehicle {
 	
 	// Fields
@@ -69,55 +67,4 @@ public class Vehicle {
 		this.wheelCount = wheelCount;
 	}
 	
-	//public static ArrayList<Vehicle>  = new ArrayList<>();
-
-	
-	//public static void addVehicle(vehicle newVehicle) {		
-		//arrayname.add("Small Espresso");
-	
-	//}
-	
-	//public static void removeVehicle(int index) {
-		
-		//arrayname.add(null);
-	//}
-	
-	//public static void updateVehicle(int index, String newOrder) {
-	
-	//System.out.println(orders);
-	
-	//orders.add(null);
-//}
-	
-	//public static void readVehicle(int index, String newOrder) {
-	
-	//System.out.println(orders);
-	
-	//orders.add(null);
-//}
-	
-	//public static void removeAllVehicle(int index) {
-		
-		//System.out.println(orders);
-		
-		//orders.add(null);
-	//}
-	
-
-//}
 }
-
-//The task is to create a garage class with methods for the following:
-//- Adding Vehicles to garage
-//- Removing vehicles
-//- Updating vehicles
-//- Reading vehicle
-//- Removing all vehicles
-//- Running vehicle.fixVehicle() custom method
-//Vehicles
-//Vehicle should be a base class (parent) with at least 3 fields
-//and a fixVehicle() method
-//
-//You should create at least 3 child classes extending off of Vehicle 
-//(car, bike, helicopter)
-//Each child class should inherit the fields AND have 2 specific fields
