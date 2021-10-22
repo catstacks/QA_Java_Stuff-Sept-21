bfd466d6013c4144b5e14efc500c010ae5da6b49
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-22 13:41:51 +0100

    Update day 5 runner

--- dfesDay5/src/com/qa/main/Runner.java
+++ dfesDay5/src/com/qa/main/Runner.java
@@ -6,13 +6,10 @@ public class Runner {
 
 	public static void main(String[] args) {
 		Herbs rosemary = new Herbs("Fully Grown", 32, "Green and slightly prickly", false, false, "Aromatic", false);
+		
 		rosemary.sway();
 		
-//		FlightLessBirds kiwi = new FlightLessBirds("Brown", 3, false, "Forest", 12);
-//		kiwi.makeNoise();
-//		kiwi.attemptToFly();
-//		
-//		System.out.println(kiwi.getFeatherColour());
+		System.out.println(rosemary.getflavour());
 
 	}
 
