9abe28eb78dfe41e3526c5d3d6b9a88b573e4b89
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-22 10:51:23 +0100

    Create child classes for Plants

--- dfesDay4/src/com/qa/main/Runner.java
+++ dfesDay4/src/com/qa/main/Runner.java
@@ -3,21 +3,21 @@ package com.qa.main;
 public class Runner {
 
 	public static void main(String[] args) {
-		//ArrayDemo_1.squareNum();
 		
-		// Plant is the data type 
-		// housePlant is the name of the object
-		// Equal to new Plant (The method we're using)
+
 				Plant housePlant = new Plant();
 				System.out.println(housePlant.lifeStage);
 				System.out.println(housePlant.height);
 				System.out.println(housePlant.leaves);
 				System.out.println(housePlant.hasBugTenants);
 				housePlant.photosynthesise();
+				
+//				FlightLessBirds kiwi = new FlightLessBirds("Brown", 3, false, "Forest", 12);
+//				kiwi.makeNoise();
+//				kiwi.attemptToFly();
+//				
+//				System.out.println(kiwi.getFeatherColour());
 
 	}
 
 }
-
-//Static methods belong to classes 
-// non static belong to objects
