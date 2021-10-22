fddb397b3a2f17c73126703f0edfa6fa4dad48a2
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-22 14:00:04 +0100

    Create vehicle parent and children classes

---
+++ dfesDay5/src/com/qa/garage/Vehicle.java
@@ -0,0 +1,50 @@
+package com.qa.garage;
+
+import java.util.ArrayList;
+
+public class Vehicle {
+	
+	
+	//public static ArrayList<vehicles>  = new ArrayList<>();
+
+	
+	//public static void add(vehicle newVehicle) {		
+		//arrayname.add("Small Espresso");
+	
+	//}
+	
+	//public static void parkVehicle(int index) {
+		
+		//arrayname.add(null);
+	//}
+	
+	//public static void removeVehicle(int index) {
+		
+		//System.out.println(orders);
+		
+		//orders.add(null);
+	//}
+	
+	//public static void updateOrder(int index, String newOrder) {
+		
+		//System.out.println(orders);
+		
+		//orders.add(null);
+	//}
+//}
+}
+
+//The task is to create a garage class with methods for the following:
+//- Adding Vehicles to garage
+//- Removing vehicles
+//- Updating vehicles
+//- Reading vehicle
+//- Removing all vehicles
+//- Running vehicle.fixVehicle() custom method
+//Vehicles
+//Vehicle should be a base class (parent) with atleast 3 fields
+//and a fixVehicle() method
+//
+//You should create at least 3 child classes extending off of Vehicle 
+//(car, bike, helicopter)
+//Each child class should inherit the fields AND have 2 specific fields
