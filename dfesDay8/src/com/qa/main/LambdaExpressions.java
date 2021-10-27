0c4064775c63f01e1c04c7897dfb880b38ea366c
catstacks <61471777+catstacks@users.noreply.github.com>
2021-10-27 13:00:40 +0100

    Create simple calculator with lambda

--- dfesDay8/src/com/qa/main/LambdaExpressions.java
+++ dfesDay8/src/com/qa/main/LambdaExpressions.java
@@ -1,6 +1,31 @@
 package com.qa.main;
 
 public class LambdaExpressions {
-	//
+	
+	interface FuncInterface {
+        double operation(double a, double b);
+    }
+
+    private double operate(double a, double b, FuncInterface funcObj) {
+        return funcObj.operation(a, b);
+    }
+
+    public static void main(String[] args) {
+        FuncInterface add = (double x, double y) -> x + y;
+
+        LambdaExpressions lambda = new LambdaExpressions();
+        System.out.println(lambda.operate(5, 7, add));
+        
+        FuncInterface subtract = (double x, double y) -> x - y;
+        System.out.println(lambda.operate(5, 7, subtract));
+        
+        FuncInterface multiply = (double x, double y) -> x * y;
+        System.out.println(lambda.operate(5, 7, multiply));
+        
+        FuncInterface divide = (double x, double y) -> x / y;
+        System.out.println(lambda.operate(5, 7, divide));
+        
+        
+    }
 
 }
