c6fe4c24cb722ef1cd7cb053ec60c1e28ce531d7
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-02 11:11:36 +0000

    Create REST app project

---
+++ StarterProject/src/main/java/com/qa/plants/rest/PlantsController.java
@@ -0,0 +1,9 @@
+package com.qa.plants.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlantsController {
+
+}
