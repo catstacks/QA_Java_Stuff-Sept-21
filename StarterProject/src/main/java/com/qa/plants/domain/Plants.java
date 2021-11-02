727e4fe4851877ae5944513ff2745704180f0aab
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-02 11:23:43 +0000

    Create Plants.java

---
+++ StarterProject/src/main/java/com/qa/plants/domain/Plants.java
@@ -0,0 +1,46 @@
+package com.qa.plants.domain;
+
+public class Plants {
+	
+	private String name;
+	private int ageInYears;
+	private boolean hasBugs;
+	
+	public Plants(String name, int ageInYears, boolean hasBugs) {
+		super();
+		this.name = name;
+		this.ageInYears = ageInYears;
+		this.hasBugs = hasBugs;
+	}
+	
+	public Plants() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAgeInYears() {
+		return ageInYears;
+	}
+
+	public void setAgeInYears(int ageInYears) {
+		this.ageInYears = ageInYears;
+	}
+
+	public boolean isHasBugs() {
+		return hasBugs;
+	}
+
+	public void setHasBugs(boolean hasBugs) {
+		this.hasBugs = hasBugs;
+	}
+	
+	
+
+}
