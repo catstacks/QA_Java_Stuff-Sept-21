2234d3333585cdb5a3afb979659b59bef014c6a4
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-04 17:14:23 +0000

    Rename to PlantsApplicationTests

--- StarterProject/src/test/java/com/qa/plants/PlantsApplicationTests.java
+++ StarterProject/src/test/java/com/qa/plants/PlantsApplicationTests.java
@@ -1,10 +1,11 @@
-package com.qa.starterproject;
+package com.qa.plants;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StarterProjectApplicationTests {
+class PlantsApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ StarterProject/src/test/java/com/qa/plants/rest/PlantIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.qa.plants.rest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.plants.domain.Plants;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PlantIntegrationTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	@Test
+	void testCreate() throws Exception {
+		Plants requestBody = new Plants("Mint", 1, true);
+		String requestBodyAsJSON = this.mapper.writeValueAsString(requestBody);
+		
+		RequestBuilder request = post("/plants/create").contentType(MediaType.APPLICATION_JSON)
+				.content(requestBodyAsJSON);
+		
+		Plants responseBody = new Plants("Mint", 1, true);
+		String responseBodyAsJSON = this.mapper.writeValueAsString(responseBody);
+
+		ResultMatcher checkStatus = status().isCreated(); 
+		ResultMatcher checkBody = content().json(responseBodyAsJSON); 
+
+		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkBody);
+
+	
+	}	
+
+}
