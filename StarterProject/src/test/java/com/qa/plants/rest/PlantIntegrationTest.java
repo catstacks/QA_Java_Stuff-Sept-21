f66dba14e10019b88f22708772390ca868a15988
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-05 11:46:40 +0000

    Update PlantIntegrationTest.java

--- StarterProject/src/test/java/com/qa/plants/rest/PlantIntegrationTest.java
+++ StarterProject/src/test/java/com/qa/plants/rest/PlantIntegrationTest.java
@@ -1,23 +1,32 @@
 package com.qa.plants.rest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.plants.domain.Plants;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(scripts = { "classpath:plants-schema.sql",
+"classpath:plants-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+
 public class PlantIntegrationTest {
 	
 	@Autowired
@@ -44,5 +53,20 @@ public class PlantIntegrationTest {
 
 	
 	}	
+	
+	@Test
+	void testGetAll() throws Exception {
+
+		RequestBuilder request = get("/plants/getAll");
+
+		ResultMatcher checkStatus = status().isOk();
+
+		Plants plant = new Plants("Mint", 1, true);
+		List<Plants> plantBuds = List.of(plant);
+		String responseBody = this.mapper.writeValueAsString(plantBuds);
+		ResultMatcher checkBody = content().json(responseBody);
+
+		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkBody);
+	}
 
 }
